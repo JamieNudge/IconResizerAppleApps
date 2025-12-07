0c7b444ad53729bb7dd4cd35d473d86058f1af98
JamieNudge <roastedtoe@googlemail.com>
2025-12-07 11:31:24 +0000

    Initial Commit

---
+++ Icon Resizer Apple AppsTests/Icon_Resizer_Apple_AppsTests.swift
@@ -0,0 +1,17 @@
+//
+//  Icon_Resizer_Apple_AppsTests.swift
+//  Icon Resizer Apple AppsTests
+//
+//  Created by Jamie on 07/12/2025.
+//
+
+import Testing
+@testable import Icon_Resizer_Apple_Apps
+
+struct Icon_Resizer_Apple_AppsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
